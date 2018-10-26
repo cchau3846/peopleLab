4152f5a088fba5bcc18c07d9311d09e15ab7d4ce
BT_1N3_17 <cchau3846@bths.edu>
2018-10-26 10:42:14 -0400

    Seating Chart

--- src/com/company/Classroom.java
+++ src/com/company/Classroom.java
@@ -1,24 +1,74 @@
 package com.company;
 
 public class Classroom {
-    Student[] students;
-    Teacher teacher;
-    public Classroom (Student[] students, Teacher teacher){
+    // Fields
+    private Student[] students;
+    private Teacher teacher;
+    private Student[][] seatingChart = new Student[6][6];
+
+    // Constructor
+    public Classroom(Student[] students, Teacher teacher) {
         this.students = students;
         this.teacher = teacher;
+        fillSeats();
     }
-    public String getSubject(){
-        return "Math";
+
+    // Methods
+
+    // Getters
+    public Student[] getStudents() {
+        return students;
     }
-    public double classAverage(){
-        double total = 0;
-        double num = 0;
-        for(int i = 0; i < this.students.length; i++){
-            total += this.students[i].getGPA();
-            num++;
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    // Setters
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    /**
+     * Adds up the GPAs of all students and divides by # of students.
+     * @return average of GPAs of students
+     */
+
+    public double classAverage() {
+        double sum = 0;
+
+        for (Student cur : students)
+            sum += cur.getGPA();
+
+        return sum / students.length;
+    }
+
+    /**
+     * @return class Teacher, then subject, then all students in the class.
+     */
+
+    public String printClass() {
+        String studentString = "";
+
+        for (int i = 0; i < students.length; i++) {
+            studentString += "\n" + (i + 1) + ". " + students[i].toString();
         }
-        double avg = total/num;
-        return avg;
-        //ooff
+
+        return "Teacher: " + teacher.toString() +
+                "\nSubject: " + teacher.getSubject() +
+                "\nStudents: " + studentString;
     }
+
+    private void fillSeats() {
+        int idx = 0;
+        for (Student[] studentArr : seatingChart) {
+            for (int i = 0; i < studentArr.length; i++) {
+                studentArr[i] = students[idx];
+                idx++;
+            }
+        }
+    }
+
 }
